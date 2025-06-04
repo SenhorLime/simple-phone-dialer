cbdd9f884557ec9a154ad2a2442e286d891cf183
João Vitor Lima <jvlime@iMac-de-Joao.local>
2025-06-04 10:33:55 -0300

    Implementação da lógica para adicionar valores ao input do número de telefone

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const dialpadButtons = document.querySelectorAll("input[type='button']");
+const phoneNumberInput = document.querySelector("input[type='number']");
 
 dialpadButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log(button.getAttribute("value"));
+    const onClickValue = button.getAttribute("value");
+    const oldInputValue = phoneNumberInput.getAttribute("value");
+
+    phoneNumberInput.setAttribute("value", oldInputValue + onClickValue);
   });
 });
