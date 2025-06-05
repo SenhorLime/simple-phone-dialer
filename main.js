1446abf0829eac6d98761598d685c33541b54aa0
João Vitor Lima <jvlime@iMac-de-Joao.local>
2025-06-05 12:16:01 -0300

    Adicionando som ao clicar no botão e mascara no input do numero de telefone

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ const dialpadButtons = document.querySelectorAll("input[type='button']");
 const phoneNumberInput = document.querySelector("input[type='text']");
 const deleteNumberButton = document.querySelector(".delete__button");
 
+const dialpadButtonSound = new Audio("/assets/dial-sound.mp3");
+
 dialpadButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const onClickValue = button.getAttribute("value");
-    const oldInputValue = phoneNumberInput.getAttribute("value");
+    const onClickPhoneNumber = phoneNumberInput.getAttribute("value");
+    const newPhoneNumber = onClickPhoneNumber + onClickValue;
 
-    phoneNumberInput.setAttribute("value", oldInputValue + onClickValue);
+    dialpadButtonSound.play();
+
+    formatPhoneNumber(newPhoneNumber);
   });
 });
 
@@ -16,3 +21,18 @@ deleteNumberButton.addEventListener("click", () => {
 
   phoneNumberInput.setAttribute("value", oldInputValue.slice(0, -1));
 });
+
+function formatPhoneNumber(phoneNumber) {
+  let cleanedPhoneNumber =phoneNumber.replace(/\D/g, "");
+
+  let formattedPhoneNumber;
+  if (cleanedPhoneNumber.length <= 2 ) {
+    formattedPhoneNumber = `(${cleanedPhoneNumber}`;
+  } else if (cleanedPhoneNumber.length <=  7) {
+    formattedPhoneNumber = `(${cleanedPhoneNumber.slice(0, 2)})${cleanedPhoneNumber.slice(2)}`;
+  } else {
+    formattedPhoneNumber = `(${cleanedPhoneNumber.slice(0, 2)})${cleanedPhoneNumber.slice(2, 7)}-${cleanedPhoneNumber.slice(7, 11)}`;
+  }
+
+  phoneNumberInput.setAttribute("value", formattedPhoneNumber);
+}
